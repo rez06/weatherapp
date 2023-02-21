a167e2cb38af26d64a4739b17e65cf7043b2948a
rez06 <pelobello.rezyl@gmail.com>
2023-02-21 21:20:58 +0800

    Added functions for date and time

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ let tempDescription = document.querySelector('#description');
 let humidityElement = document.querySelector('#humidity');
 let windElement = document.querySelector('#wind')
 
+// variables for date
+let dateElement = document.querySelector('#date');
+
+
+// function for Dates
+function formatDate(timestamp) {
+    //calculate the date
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+
+     if (hours < 10) {
+        hours = `0${hours}`;
+     }
+    
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
     temperatureEl.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -14,7 +40,8 @@ function displayTemperature(response) {
     console.log(response.data);
     tempDescription.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = response.data.wind.speed
+    windElement.innerHTML = response.data.wind.speed;
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 axios.get(apiUrl).then(displayTemperature)
