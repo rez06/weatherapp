02a0554ca2fb003c3d524d53dcfcdf20b1d44603
rez06 <pelobello.rezyl@gmail.com>
2023-02-21 23:05:06 +0800

    Added the relevant icons from the weather API

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let countryElement = document.querySelector('#country');
 let tempDescription = document.querySelector('#description');
 let humidityElement = document.querySelector('#humidity');
 let windElement = document.querySelector('#wind')
+let iconElement = document.querySelector('#icon');
 
 // variables for date
 let dateElement = document.querySelector('#date');
-
 
 // function for Dates
 function formatDate(timestamp) {
@@ -33,15 +33,17 @@ function formatDate(timestamp) {
 
 }
 
+
 function displayTemperature(response) {
     temperatureEl.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     countryElement.innerHTML = response.data.sys.country;
-    console.log(response.data);
     tempDescription.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
-    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    dateElement.innerHTML = formatDate(response.data.dt * 1000); 
+    iconElement.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
+    iconElement.setAttribute('alt', response.data.weather[0].description);
 }
 
 axios.get(apiUrl).then(displayTemperature)
