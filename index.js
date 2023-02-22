8f7e975016cf8a11986d676a52e7ec582628e5ee
rez06 <pelobello.rezyl@gmail.com>
2023-02-22 15:32:30 +0800

    Added unit conversion for celsius and farenheit and add active link between the celsius and fareheit temp

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let apiKey = '060c7c61814c1dd7e9362ab95b9026ea';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=${apiKey}`;
 let temperatureEl = document.querySelector('#temperature');
 let cityElement = document.querySelector('#city');
 let countryElement = document.querySelector('#country');
@@ -7,16 +5,23 @@ let tempDescription = document.querySelector('#description');
 let humidityElement = document.querySelector('#humidity');
 let windElement = document.querySelector('#wind')
 let iconElement = document.querySelector('#icon');
+let celsiusTemp = null;
+
+
+
+
+//variables for form
+let form = document.querySelector('.search-form');
+form.addEventListener('submit', handleSubmit)
 
 // variables for date
 let dateElement = document.querySelector('#date');
-
+    
 // function for Dates
 function formatDate(timestamp) {
     //calculate the date
     let date = new Date(timestamp);
     let hours = date.getHours();
-
      if (hours < 10) {
         hours = `0${hours}`;
      }
@@ -26,24 +31,63 @@ function formatDate(timestamp) {
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-
     let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
+}
+
+function search(city) {
+    // let city = 'Manila';
+    let apiKey = '060c7c61814c1dd7e9362ab95b9026ea';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayTemperature);
 
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector('#city-input');
+    search(cityInputElement.value);    
+}
+
+
+//variables for unit conversion
+let farenheitLink = document.querySelector('.farenheit');
+farenheitLink.addEventListener('click', displayFarenheitTemp);
+
+let celsiusLink = document.querySelector('.celsius');
+celsiusLink.addEventListener('click', displayCelsiusTemp);
+
+function displayCelsiusTemp(event) {
+    event.preventDefault();
+    farenheitLink.classList.remove('active')
+    celsiusLink.classList.add('active');
+    temperatureEl.innerHTML = Math.round(celsiusTemp);
+}
+
+function displayFarenheitTemp(event) {
+    event.preventDefault();
+    let farenheitTemp = (celsiusTemp * 9) / 5 + 32;
+    //remove the celsius active link
+    celsiusLink.classList.remove('active');
+    farenheitLink.classList.add('active')
+    temperatureEl.innerHTML = Math.round(farenheitTemp);
+}
 
 function displayTemperature(response) {
-    temperatureEl.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemp = response.data.main.temp;
+    temperatureEl.innerHTML = Math.round(celsiusTemp);
     cityElement.innerHTML = response.data.name;
     countryElement.innerHTML = response.data.sys.country;
     tempDescription.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
     dateElement.innerHTML = formatDate(response.data.dt * 1000); 
+   
     iconElement.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
-    iconElement.setAttribute('alt', response.data.weather[0].description);
+    iconElement.setAttribute('alt', response.data.weather[0].description);    
 }
 
-axios.get(apiUrl).then(displayTemperature)
+
+
+   
